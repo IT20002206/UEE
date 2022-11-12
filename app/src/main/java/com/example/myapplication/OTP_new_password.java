ae76da0fad88083915d11a2b40b26c428ba78513
Tharmithan N <kumartharmi98@gmail.com>
2022-11-12 16:23:35 +0530

    OTP new password page added

---
+++ app/src/main/java/com/example/myapplication/OTP_new_password.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTP_new_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_new_password);
+    }
+}
