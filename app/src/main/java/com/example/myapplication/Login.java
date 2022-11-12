158642015268b8115c50557cf5bfa4323a58377e
Tharmithan N <kumartharmi98@gmail.com>
2022-11-12 21:23:45 +0530

    burger button initial page created

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,13 +2,51 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
+
+    ImageButton myImageButton;
+    ImageButton loginImage;
+    ImageButton idontImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        myImageButton = (ImageButton) findViewById(R.id.forgot_btn);
+
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Login.this, Reset_Password.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        loginImage = (ImageButton) findViewById(R.id.imageButton5);
+
+        loginImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Login.this, Weather.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        idontImage = (ImageButton) findViewById(R.id.idont);
+
+        idontImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Login.this, Sign_Up.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
