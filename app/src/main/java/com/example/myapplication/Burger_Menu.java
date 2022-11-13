a5adc4e242b36897a6dd5b6a0d3c18568ac44427
Tharmithan N <kumartharmi98@gmail.com>
2022-11-13 09:55:02 +0530

    burger Button prototyping

--- app/src/main/java/com/example/myapplication/Burger_Menu.java
+++ app/src/main/java/com/example/myapplication/Burger_Menu.java
@@ -10,6 +10,12 @@ import android.widget.ImageButton;
 public class Burger_Menu extends AppCompatActivity {
 
     ImageButton Burger_Close;
+    ImageButton Burger_Home;
+    ImageButton Burger_Weather;
+    ImageButton Burger_Vehicle;
+    ImageButton Burger_News;
+    ImageButton Burger_Recycle;
+    ImageButton Burger_LogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,66 @@ public class Burger_Menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Weather.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_Home = (ImageButton) findViewById(R.id.imageButton13);
+
+        Burger_Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Weather.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_Weather = (ImageButton) findViewById(R.id.imageButton15);
+
+        Burger_Weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Weather.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_Vehicle = (ImageButton) findViewById(R.id.imageButton16);
+
+        Burger_Vehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Vehicle.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_News = (ImageButton) findViewById(R.id.imageButton18);
+
+        Burger_News.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, News_home.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_Recycle = (ImageButton) findViewById(R.id.imageButton49);
+
+        Burger_Recycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Recycle_First.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        Burger_LogOut = (ImageButton) findViewById(R.id.imageButton19);
+
+        Burger_LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Burger_Menu.this, Login.class);
                 startActivity(intentLoadNewActivity);
             }
         });
--- app/src/main/java/com/example/myapplication/Sign_Up.java
+++ app/src/main/java/com/example/myapplication/Sign_Up.java
@@ -2,13 +2,39 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Sign_Up extends AppCompatActivity {
+
+    ImageButton registerBTN;
+    ImageButton clickRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        registerBTN = (ImageButton) findViewById(R.id.i_have);
+
+        registerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Sign_Up.this, Login.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        clickRegister = (ImageButton) findViewById(R.id.i_have);
+
+        clickRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity = new Intent(Sign_Up.this, Login.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
     }
 }
